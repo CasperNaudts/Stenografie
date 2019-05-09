1697edec6591833978856906458d620539ae9b9f
Casper Naudts <casper@edu-tech.be>
2019-05-09 11:04:42 +0200

    finish stenografie

---
+++ Crypto.Gui/MainWindow.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using folder = System.Windows.Forms;
+using Crypto.Stenografie;
+
+namespace Crypto.Gui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private OpenFileDialog browseVenster = new OpenFileDialog();
+        private String _imagePath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        #region Stenografie
+        private void browseImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            browseVenster.Filter = "Image Files (*.jpeg; *.png; *.bmp)| *.jpg; *.png; *.bmp";
+            if (browseVenster.ShowDialog() == true)
+            {
+                string padFoto = browseVenster.FileName;
+                //FileNameLabel.Content = selectedFileName;
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(padFoto);
+                bitmap.EndInit();
+                SelectedImage.Source = bitmap;
+                // labelSelectedImage.Content = padFoto;
+                _imagePath = padFoto;
+            }
+        }
+
+        private void HideData(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string text = RichTextBox.Text;
+
+                // string padFoto = labelSelectedImage.Content.ToString();
+                Bitmap bitmap = new Bitmap(_imagePath);
+
+                if (text.Equals(""))
+                {
+                    MessageBox.Show("The text you want to hide can't be empty", "Warning");
+                    return;
+                }
+
+                if (EncrypedCheckBox.IsChecked == true)
+                {
+                    if (PasswordPassword.Password.Length < 6)
+                    {
+                        MessageBox.Show("Please enter a password with at least 6 characters", "Warning");
+                        return;
+                    }
+                    else
+                    {
+                        text = StenografieCrypto.EncryptStringAes(text, PasswordPassword.Password);
+                    }
+                }
+
+                bitmap = StenografieHelper.EmbedText(text, bitmap);
+
+                MessageBox.Show("Your text was hidden in the image successfully!", "Done");
+                MaakLeeg();
+
+                SaveFileDialog saveDialog = new SaveFileDialog();
+                saveDialog.Filter = "Png Image|*.png|Bitmap Image|*.bmp";
+
+                if (saveDialog.ShowDialog() == true)
+                {
+                    switch (saveDialog.FilterIndex)
+                    {
+                        case 0:
+                        {
+                            bitmap.Save(saveDialog.FileName, ImageFormat.Png);
+                        }
+                            break;
+                        case 1:
+                        {
+                            bitmap.Save(saveDialog.FileName, ImageFormat.Bmp);
+                        }
+                            break;
+                    }
+
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Something went wrong");
+            }
+            finally
+            {
+                MaakLeeg();
+            }
+        }
+
+        /// <summary>
+        /// Maar de invoervelden leeg
+        /// </summary>
+        private void MaakLeeg()
+        {
+            RichTextBox.Text = "";
+            PasswordPassword.Password = "";
+            EncrypedCheckBox.IsChecked = false;
+        }
+        #endregion
+
+        private void discoverButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string padFoto = browseVenster.FileName;
+                Bitmap bitmap = new Bitmap(padFoto);
+
+                string extractedText = StenografieHelper.ExtractText(bitmap);
+
+                if (EncrypedCheckBox.IsChecked == true)
+                {
+                    try
+                    {
+                        extractedText = StenografieCrypto.DecryptStringAes(extractedText, PasswordPassword.Password);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Wrong password", "Error");
+
+                        return;
+                    }
+                }
+                RichTextBox.Text = extractedText;
+            }
+            catch
+            {
+                MessageBox.Show("Something went wrong");
+            }
+        }
+    }
+    
+}
---
+++ Crypto.Stenografie/StenografieHelper.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Drawing;
+
+namespace Crypto.Stenografie
+{
+    public class StenografieHelper
+    {
+        public enum Opdracht
+        {
+            Verbergen,
+            VullenMetNullen
+        };
+
+        /// <summary>
+        /// TODO: een regel tekst verbergen in een afbeelding dmv een kleine aanpassing van pixels
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="afbeelding"></param>
+        /// <returns></returns>
+        public static Bitmap EmbedText(string text, Bitmap afbeelding)
+        {
+            Opdracht opdracht = Opdracht.Verbergen;
+            int letterPositie = 0;
+            int letterWaarde = 0;
+            long pixelPositie = 0;
+            int nullen = 0;
+            int R, G, B;
+
+            for (int i = 0; i < afbeelding.Height; i++)
+            {
+                for (int j = 0; j < afbeelding.Width; j++)
+                {
+                    Color pixel = afbeelding.GetPixel(j, i);
+
+                    R = pixel.R - pixel.R % 2;
+                    G = pixel.G - pixel.G % 2;
+                    B = pixel.B - pixel.B % 2;
+
+                    for (int n = 0; n < 3; n++)
+                    {
+                        if (pixelPositie % 8 == 0)
+                        {
+                            if (opdracht == Opdracht.VullenMetNullen && nullen == 8)
+                            {
+                                if ((pixelPositie - 1) % 3 < 2)
+                                {
+                                    afbeelding.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+                                return afbeelding;
+                            }
+
+                            if (letterPositie >= text.Length)
+                            {
+                                opdracht = Opdracht.VullenMetNullen;
+                            }
+                            else
+                            {
+                                letterWaarde = text[letterPositie++];
+                            }
+                        }
+
+                        switch (pixelPositie % 3)
+                        {
+                            case 0:
+                                {
+                                    if (opdracht == Opdracht.Verbergen)
+                                    {
+                                        R += letterWaarde % 2;
+
+                                        letterWaarde /= 2;
+                                    }
+                                }
+                                break;
+                            case 1:
+                                {
+                                    if (opdracht == Opdracht.Verbergen)
+                                    {
+                                        G += letterWaarde % 2;
+
+                                        letterWaarde /= 2;
+                                    }
+                                }
+                                break;
+                            case 2:
+                                {
+                                    if (opdracht == Opdracht.Verbergen)
+                                    {
+                                        B += letterWaarde % 2;
+
+                                        letterWaarde /= 2;
+                                    }
+
+                                    afbeelding.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+                                break;
+                        }
+
+                        pixelPositie++;
+
+                        if (opdracht == Opdracht.VullenMetNullen)
+                        {
+                            nullen++;
+                        }
+                    }
+                }
+            }
+            return afbeelding;
+        }
+
+        public static string ExtractText(Bitmap bmp)
+        {
+            int colorUnitIndex = 0;
+            int charValue = 0;
+
+            string extractedText = String.Empty;
+
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    Color pixel = bmp.GetPixel(j, i);
+
+                    for (int n = 0; n < 3; n++)
+                    {
+                        switch (colorUnitIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    charValue = charValue * 2 + pixel.R % 2;
+                                }
+                                break;
+                            case 1:
+                                {
+                                    charValue = charValue * 2 + pixel.G % 2;
+                                }
+                                break;
+                            case 2:
+                                {
+                                    charValue = charValue * 2 + pixel.B % 2;
+                                }
+                                break;
+                        }
+
+                        colorUnitIndex++;
+
+                        if (colorUnitIndex % 8 == 0)
+                        {
+                            charValue = ReverseBits(charValue);
+
+                            if (charValue == 0)
+                            {
+                                return extractedText;
+                            }
+
+                            char c = (char)charValue;
+
+                            extractedText += c.ToString();
+                        }
+                    }
+                }
+            }
+
+            return extractedText;
+        }
+
+        public static int ReverseBits(int n)
+        {
+            int result = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                result = result * 2 + n % 2;
+
+                n /= 2;
+            }
+
+            return result;
+        }
+    }
+}
